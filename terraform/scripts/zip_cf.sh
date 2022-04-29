93997786023c82f5b3218a0ac14809c53472b6c4
Giovani Kisz <giovani.kisz@ibm.com>
2022-04-29 14:30:58 -0300

    Changes

--- terraform/scripts/zip_cf.sh
+++ terraform/scripts/zip_cf.sh
@@ -1,2 +1,7 @@
+# Install zip
+# yum install zip unzip -y
+
 # Creates a zip file to the referenced Cloud Function
-zip -r ../cloud-functions/$FILENAME.zip $FILEPATH
+# zip -r ../cloud-functions/$FILENAME.zip $FILEPATH
+
+pwd
